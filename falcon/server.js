aef84d5e0271421e76e246e50d5f90a82c6f3367
Arkadiusz Pasek <arkadiusz.pasek99@gmail.com>
2021-04-23 13:29:36 +0200

    Docker

---
+++ falcon/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
